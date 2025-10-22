26bd76942f8e3f2bdcddaa37c0e223c04271c865
Syed zainul abedin <szsimplesyed93@gmail.com>
2025-10-22 10:22:02 +0530

    WIP

---
+++ Assets/Scripts/Systems/Gameplay/BoxColorSystem.cs
@@ -0,0 +1,36 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Physics;
+
+namespace Systems.Gameplay
+{
+    public partial class BoxColorSystem : SystemBase
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            
+        }
+
+        protected override void OnUpdate()
+        {
+            var entityCommandBuffer = new EntityCommandBuffer(Allocator.Temp);
+
+            foreach (var (physicsMass, healthComponent, entity) in SystemAPI.Query<RefRW<PhysicsMass>, RefRW<HealthComponent>>().WithEntityAccess())
+            {
+                if (healthComponent.ValueRO.CurrentHealth <= 100 && healthComponent.ValueRO.CurrentHealth > 50)
+                {
+                    
+                }
+                else if (healthComponent.ValueRO.CurrentHealth <= 50 && healthComponent.ValueRO.CurrentHealth > 0)
+                {
+                    
+                }
+                else if (healthComponent.ValueRO.CurrentHealth <= 0)
+                {
+                    physicsMass.ValueRW.InverseMass = 0f;
+                }
+            }
+        }
+    }
+}
