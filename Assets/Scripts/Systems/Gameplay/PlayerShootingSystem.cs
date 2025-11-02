88b6aa31312859174712c6cfd23e0807b33c1b75
Syed zainul abedin <szsimplesyed93@gmail.com>
2025-11-03 00:08:48 +0530

    Fixed the issue with player shooting

--- Assets/Scripts/Systems/Gameplay/PlayerShootingSystem.cs
+++ Assets/Scripts/Systems/Gameplay/PlayerShootingSystem.cs
@@ -26,21 +26,22 @@ namespace Systems.Gameplay
         {
             var physicsWorld = SystemAPI.GetSingleton<PhysicsWorldSingleton>().PhysicsWorld;
             var commandBuffer = new EntityCommandBuffer(Allocator.Temp);
-            foreach (var (player, inputData, transform) in SystemAPI.Query<RefRW<PlayerData>, RefRW<PlayerInputData>, RefRO<LocalTransform>>())
+            foreach (var (player, inputData, transform, entity) in SystemAPI.Query<RefRW<PlayerData>, RefRW<PlayerInputData>, RefRO<LocalTransform>>().WithEntityAccess())
             {
                 if (inputData.ValueRO.shoot.IsSet)
                 {
-                    RaycastHit hit;
+                    var allHits = new NativeList<RaycastHit>(Allocator.Temp);
                     var rayOrigin = transform.ValueRO.Position + new float3(0, 1.5f, 0);
                     var rayDirection = transform.ValueRO.Forward();
                     
                     // TODO: Move this to a config variable,
                     var filter = new CollisionFilter
                     {
-                        BelongsTo = (uint)1 << 1,
+                        BelongsTo = (uint) 1 << 0,
                         CollidesWith = (uint) 1 << 6 | (uint) 1 << 0,
                         GroupIndex = 0
                     };
+                    
                     var raycastInput = new RaycastInput
                     {
                         Start = rayOrigin,
@@ -49,40 +50,54 @@ namespace Systems.Gameplay
                     };
                     Debug.DrawRay(rayOrigin, rayDirection * 100f, Color.red, 0.1f);
 
-                    if (physicsWorld.CastRay(raycastInput, out hit))
+                    if (physicsWorld.CastRay(raycastInput, ref allHits))
                     {
-                        var hitEntity = hit.Entity;
-                        if (SystemAPI.HasComponent<GhostOwnerIsLocal>(hitEntity))
+                        foreach (var hit in allHits)
                         {
-                            continue;
-                        }
-                        if (!SystemAPI.HasComponent<HealthComponent>(hitEntity))
-                        {
-                            continue;
-                        }
-                        var hitHealth = SystemAPI.GetComponent<HealthComponent>(hitEntity);
-                        if (hitHealth.IsAlive)
-                        {
-                            // Choosing 34 as the damage value for the hit
-                            hitHealth.CurrentHealth = (ushort)math.max(0, hitHealth.CurrentHealth - 34);
-                            state.EntityManager.SetComponentData(hitEntity, hitHealth);
-                            
-                            if (hitHealth.CurrentHealth <= 0)
-                            {
-                                if (SystemAPI.HasComponent<BoxComponent>(hitEntity))
-                                {
-                                    state.EntityManager.SetComponentData(hitEntity, new BoxComponent
-                                    {
-                                        toDestroy = true
-                                    });
-                                }
-                            }
+                            ProcessHit(entity, hit, ref state, player);
                         }
                     }
                 }
             }
             commandBuffer.Playback(state.EntityManager);
             commandBuffer.Dispose();
+        }
+
+        private void ProcessHit(Entity sourceEntity, RaycastHit hit, ref SystemState state, RefRW<PlayerData> player)
+        {
+            var hitEntity = hit.Entity;
+
+            if (hitEntity == sourceEntity)
+            {
+                return;
+            }
+            // if (SystemAPI.HasComponent<GhostOwnerIsLocal>(hitEntity))
+            // {
+            //     return;
+            // }
+            if (!SystemAPI.HasComponent<HealthComponent>(hitEntity))
+            {
+                return;
+            }
+
+            var hitHealth = SystemAPI.GetComponent<HealthComponent>(hitEntity);
+            if (hitHealth.IsAlive)
+            {
+                // Choosing 34 as the damage value for the hit
+                hitHealth.CurrentHealth = (ushort)math.max(0, hitHealth.CurrentHealth - 34);
+                state.EntityManager.SetComponentData(hitEntity, hitHealth);
+                            
+                if (hitHealth.CurrentHealth <= 0)
+                {
+                    if (SystemAPI.HasComponent<BoxComponent>(hitEntity))
+                    {
+                        state.EntityManager.SetComponentData(hitEntity, new BoxComponent
+                        {
+                            toDestroy = true
+                        });
+                    }
+                }
+            }
         }
     }
 }
